5dcfd8b1365a07c889201a17146cecc5025202e9
La-Kiks <kilian.audroin@gmail.com>
2025-12-09 16:10:54 +0100

    Landing part 1

--- ui/src/app/pages/landing/landing.component.ts
+++ ui/src/app/pages/landing/landing.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../components/ui/button/button.component';
 import { CardComponent } from '../../components/ui/card/card.component';
-import { ScrollComponent } from '../../components/ui/scroll/scroll.component';
 
 
 @Component({
   selector: 'app-landing',
-  imports: [ButtonComponent, CardComponent, ScrollComponent],
+  imports: [ButtonComponent, CardComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
