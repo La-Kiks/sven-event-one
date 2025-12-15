228811e8e80437a2104334368d2645362490da09
La-Kiks <kilian.audroin@gmail.com>
2025-12-15 16:07:05 +0100

    Form creation

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: LandingComponent },
+    { path: "inscription", component: InscriptionComponent },
+];
---
+++ ui/src/app/pages/inscription/inscription.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscription',
+  imports: [],
+  templateUrl: './inscription.component.html',
+  styleUrl: './inscription.component.scss'
+})
+export class InscriptionComponent {
+  // document.querySelector('form').addEventListenner('submit', function(e) {
+  //   e.preventDefault();
+  //   alert('Form submitted successfully! (This is a demo - no data was actually sent)');
+  // });
+
+}
