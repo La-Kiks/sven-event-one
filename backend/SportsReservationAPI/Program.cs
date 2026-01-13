da715eb2ea85098b173abb55b26a5f7c5f12f265
La-Kiks <kilian.audroin@gmail.com>
2026-01-13 18:12:46 +0100

    Validator

--- backend/SportsReservationAPI/Program.cs
+++ backend/SportsReservationAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using FluentValidation.AspNetCore;
 using SportsReservationAPI.Models;
 
 
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ReservationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ReservationDatabase")));
+
+builder.Services.AddFluentValidationAutoValidation();
 
 var app = builder.Build();
 
