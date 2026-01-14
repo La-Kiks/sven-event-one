8dd520a0117c63b2c90bae9864eac47a49905ade
La-Kiks <kilian.audroin@gmail.com>
2026-01-14 12:15:42 +0100

    api url placeholder

--- ui/src/app/services/stripe.service.ts
+++ ui/src/app/services/stripe.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { firstValueFrom } from "rxjs";
-import { environment } from "../../environment.development";
+import { environment } from "../../environment";
 
 @Injectable({
     providedIn: 'root'
