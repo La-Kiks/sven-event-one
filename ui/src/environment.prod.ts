39294e4406b5ad964383bdec394556e42318153e
La-Kiks <kilian.audroin@gmail.com>
2026-03-12 12:18:51 +0100

    feat: update environment configuration for development and production

--- ui/src/environment.prod.ts
+++ ui/src/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     apiUrl: 'http://localhost:5000',
     stripePublishableKey: 'pk_test_your_test_key'
 };
--- ui/src/environment.ts
+++ ui/src/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
-    production: true,
-    // apiUrl: 'http://localhost:5000',
+    production: false,
     apiUrl: 'http://localhost:5148',
     stripePublishableKey: 'pk_test_your_test_key'
-    // apiUrl: 'https://kilian-au.fr',
 };
