3e0cedf64f7dda3ba4943adf7f4e22b5a597fd72
La-Kiks <kilian.audroin@gmail.com>
2026-03-13 09:16:38 +0000

    Not Secret Prod change

--- ui/src/environment.ts
+++ ui/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5148',
-    stripePublishableKey: 'pk_test_your_test_key'
+    production: true,
+    apiUrl: 'https://sport-challenge-police-54.fr',
+    stripePublishableKey: 'pk_test_51SmuSe1fMF30f42G7SdPgpxBRFYF4QUOrSdgDdbTYNGnHKCL3ObYCbYaAfjwPqnflhaOkWppq4uPI4UkS2lpnFyb001dwcIIY9'
 };
